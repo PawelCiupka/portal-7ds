016ef2d5a9f57daf2eb4b39dade23ba8c0cb85cf
pawel.ciupka <pawel.ciupka@accenture.com>
2019-11-12 14:46:23 +0100

    heroku configuration: get config values from heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import session from "express-session";
 import connectStore from "connect-mongo";
 const logger = require("morgan");
+const aws = require("aws-sdk");
 import {
   userRoutes,
   sessionRoutes,
@@ -20,7 +21,17 @@ import {
 
 (async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
+    let s3 = new aws.S3({
+        PORT = process.env.PORT,
+        NODE_ENV = process.env.NODE_ENV,
+        MONGO_URI = process.env.MONGO_URI,
+        SESS_NAME = process.env.SESS_NAME,
+        SESS_SECRET = process.env.SESS_SECRET,
+        SESS_LIFETIME = process.env.SESS_LIFETIME
+    })
+
+
+    await mongoose.connect(s3.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
@@ -40,19 +51,19 @@ import {
 
     app.use(
       session({
-        name: SESS_NAME,
-        secret: SESS_SECRET,
+        name: s3.SESS_NAME,
+        secret: s3.SESS_SECRET,
         store: new MongoStore({
           mongooseConnection: mongoose.connection,
           collection: "session",
-          ttl: parseInt(SESS_LIFETIME) / 1000
+          ttl: parseInt(s3.SESS_LIFETIME) / 1000
         }),
         saveUninitialized: false,
         resave: false,
         cookie: {
           sameSite: true,
-          secure: NODE_ENV === "production",
-          maxAge: parseInt(SESS_LIFETIME)
+          secure: s3.NODE_ENV === "production",
+          maxAge: parseInt(s3.SESS_LIFETIME)
         }
       })
     );
@@ -64,7 +75,7 @@ import {
     apiRouter.use("/ticket", ticketRoutes);
     apiRouter.use("/management", managementRoutes);
 
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    app.listen(s3.PORT, () => console.log(`Listening on port ${s3.PORT}`));
   } catch (err) {
     console.log(err);
   }
