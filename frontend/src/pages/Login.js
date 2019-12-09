d6b73ef3b5f3c43e8f3b6fa65534fe5253144e01
pawelciupka <pawelciupka@gmail.com>
2019-12-09 13:25:08 +0100

    create prototype of login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import UserManagementLoginForm from "../components/userManagement/loginForm";
 
 const Login = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 922);
+
+  window.addEventListener("resize", () => {
+    setIsMobile(window.innerWidth <= 768);
+  });
+
   return (
     <>
-      <Container className="login-container">
-        <div className="logo-container">
-          <h1>Portal 7DS</h1>
-        </div>
-        <UserManagementLoginForm />
-        <div className="link-to-signup-container">
-          <p>
-            Nie masz konta? <Link to="/signup">Zarejestruj się</Link>
-          </p>
-        </div>
-      </Container>
+      {" "}
+      {isMobile ? (
+        <Container className="login-container">
+          <div className="logo-container">
+            <h1>Portal 7DS</h1>
+          </div>
+          <UserManagementLoginForm />
+          <div className="link-to-signup-container">
+            <p>
+              Nie masz konta? <Link to="/signup">Zarejestruj się</Link>
+            </p>
+          </div>
+        </Container>
+      ) : (
+        <>
+          <Container className="login-container">
+            <div className="login-div-left">
+              <div className="logo-container">
+                <h1>Portal 7DS</h1>
+              </div>
+            </div>
+            <div className="login-div-right">
+              <UserManagementLoginForm />
+              <div className="link-to-signup-container">
+                <p>
+                  Nie masz konta? <Link to="/signup">Zarejestruj się</Link>
+                </p>
+              </div>
+            </div>
+          </Container>
+        </>
+      )}{" "}
     </>
   );
 };
