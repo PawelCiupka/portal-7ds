c4c6c1b00351913184ec5f313b4d8271bb0d03ae
pawelciupka <pawelciupka@gmail.com>
2019-12-21 20:54:40 +0100

    upgrade styles for room reservation menu

--- frontend/src/components/roomReservation/roomSelectCard.js
+++ frontend/src/components/roomReservation/roomSelectCard.js
@@ -1,24 +1,18 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 const RoomReservationRoomSelectCard = props => {
   return (
     <>
-      <Card className="reservation-card-container">
+      <Card
+        className="reservation-card-container"
+        onClick={props.reservationFunc}
+      >
         <div className="reservation-card-color" />
         <Card.Body>
           <div className="reservation-card-menu-wrapper">
             <Card.Title>{props.title}</Card.Title>
-            <div className="reservice-card-buttons-wrapper">
-              <Button variant="primary" onClick={props.informationFunc}>
-                Informacje
-              </Button>
-              <br />
-              <Button variant="primary" onClick={props.reservationFunc}>
-                Rezerwuj
-              </Button>
-            </div>
           </div>
         </Card.Body>
       </Card>
@@ -29,7 +23,5 @@ const RoomReservationRoomSelectCard = props => {
 export default RoomReservationRoomSelectCard;
 
 RoomReservationRoomSelectCard.prototype = {
-  imageSrc: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string
+  title: PropTypes.string.isRequired
 };
